ce0206099706e23d9b649df7cd0295cf20ebefd7
wanghao123-dev <easy_hongliang@163.com>
2024-12-24 14:39:19 +0800

    Update copy_manga.js
    update

--- copy_manga.js
+++ copy_manga.js
@@ -8,7 +8,7 @@ class CopyManga extends ComicSource {
 
     minAppVersion = "1.0.0"
 
-    url = "https://raw.githubusercontent.com/venera-app/venera-configs/refs/heads/main/copy_manga.js"
+    url = "https://raw.githubusercontent.com/wanghao123-dev/venera-configs/refs/heads/main/copy_manga.js"
 
     headers = {}
 
