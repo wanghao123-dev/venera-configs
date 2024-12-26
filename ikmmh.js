3fb25a7ffb586e632af55f086432aad4937512b3
wanghao123-dev <easy_hongliang@163.com>
2024-12-26 17:20:15 +0800

    Update ikmmh.js

--- ikmmh.js
+++ ikmmh.js
@@ -26,7 +26,7 @@ class NewComicSource extends ComicSource {  // 首行必须为class...
         login: async (account, pwd) => {
             let res = await Network.post("https://ymcdnyfqdapp.ikmmh.com/api/user/userarr/login", {
                 "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
-                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36"
+                "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 16_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.6 Mobile/15E148 Safari/604.1"
             }, `user=${account}&pass=${pwd}`)
 
             let data = JSON.parse(res.body)
